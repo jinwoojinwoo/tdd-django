7949b639032d91d13e1cd357c275d8b0f4dbdd37
jin-choi <jin.choi@daumkakao.com>
2018-07-22 18:18:54 +0900

    ft 테스트 추가, 2개의 row 추가

--- lists/tests.py
+++ lists/tests.py
@@ -7,14 +7,12 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
 
-    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': '신규 작업 아이템'})
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
