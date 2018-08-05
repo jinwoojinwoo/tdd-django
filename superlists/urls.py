9f106c36452ab23bd892858ec840e401a40c816e
jin-choi <jin.choi@daumkakao.com>
2018-08-05 13:47:02 +0900

    두 개 폼을 위한 url적용, views와 tests에 있는 코드 이동, 신규 url

--- superlists/urls.py
+++ superlists/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     #path('admin/', admin.site.urls),
     path('', list_views.home_page, name='home'),
     path('lists/the-only-list-in-the-world/', list_views.view_list, name='view_list'),
+    path('lists/new', list_views.new_list, name='new_list'),
 ]
