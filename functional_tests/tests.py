ebc8c795471ff45a4a4b287dcdd7256e1b7ce1cf
jin-choi <jin.choi@daumkakao.com>
2018-08-05 21:50:52 +0900

    레이아웃 개선을 위한 부트스트랩 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -92,10 +92,11 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 둘 다 만족하고 잠자리에 든다
 
-    def test_layout_and_styling(self):
+    def _test_layout_and_styling(self):
         # 에디스는 메인 페이지를 방문한다
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
+        time.sleep(2)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
