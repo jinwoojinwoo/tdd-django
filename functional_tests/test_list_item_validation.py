9ccae1268419808768454ba8974806dcd17e0921
jin-choi <jin.choi@daumkakao.com>
2018-08-29 23:10:48 +0900

    모델 검증을 위한 신규 목록 뷰 수정

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,7 +8,8 @@ class ItemValidationTest(FunctionalTest):
         # 에디스는 메인 페이지에 접속해서 빈 아이템을 실수로 등록하려고 한다
         # 입력 상자가 비어 있는 상태에서 엔터키를 누른다
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_new_item').send_keys('\n'); time.sleep(1)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        time.sleep(2)
 
         # 페이지가 새로고침되고, 빈 아이템을 등록할 수 없다는 
         # 에러 메세지가 표시된다
@@ -16,11 +17,14 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, '빈 아이템을 동록할 수 없습니다')
 
         # 다른 아이템을 입력하고 이번에는 정상 처리된다
-        self.browser.find_element_by_id('id_new_item').send_keys('우유 사기\n'); time.sleep(1)
+        self.browser.find_element_by_id('id_new_item').send_keys('우유 사기\n')
+        time.sleep(1)
+
         self.check_for_row_in_list_table('1: 우유 사기')
 
         # 그녀는 고의적으로 다시 빈 아이템을 등록한다
-        self.browser.find_element_by_id('id_new_item').send_keys('\n'); time.sleep(1)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        time.sleep(1)
 
         # 리스트 페이지에 다시 에러 메세지가 표시된다
         self.check_for_row_in_list_table('1: 우유 사기')
@@ -28,7 +32,9 @@ class ItemValidationTest(FunctionalTest):
         self.assertEqual(error.text, '빈 아이템을 동록할 수 없습니다')
 
         # 아이템을 입력하면 정상 동작한다
-        self.browser.find_element_by_id('id_new_item').send_keys('tea 만들기\n'); time.sleep(1)
+        self.browser.find_element_by_id('id_new_item').send_keys('tea 만들기\n')
+        time.sleep(1)
+
         self.check_for_row_in_list_table('1: 우유 사기')
         self.check_for_row_in_list_table('2: tea 만들기')
 
