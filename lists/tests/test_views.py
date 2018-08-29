981572e8d31b5181718d28fd1a82b6380dda888d
jin-choi <jin.choi@daumkakao.com>
2018-08-29 23:38:05 +0900

    리스트 뷰에서 유효성 검증 구현

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -92,3 +92,14 @@ class ListViewTest(TestCase):
         )
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id,),
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = "빈 아이템을 등록할 수 없습니다"
+        self.assertContains(response, expected_error)
