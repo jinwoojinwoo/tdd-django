8b18c8f3cb12ad14264d668389ed693d3b80699f
jin-choi <jin.choi@daumkakao.com>
2019-03-01 13:15:33 +0900

    모든 아이템의 입력 id와 name 변경. 아직 망가진 상태

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -12,7 +12,7 @@ class LayoutAndStylingTest(FunctionalTest):
         self.browser.set_window_size(1024, 768)
         time.sleep(2)
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box() 
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width']/2,
             512,
@@ -24,7 +24,7 @@ class LayoutAndStylingTest(FunctionalTest):
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box() 
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width']/2,
             512,
